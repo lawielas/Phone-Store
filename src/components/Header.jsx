72fb1a3075d2fbbfc979cc978b72cd588bb75bf6
lawielas <lawie.aminu@yahoo.com>
2023-09-10 21:19:01 +0100

    header complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import headerLaptop from "../assets/images/laptop-removebg-preview.png"
 
 
 export const Header = () => {
@@ -22,25 +23,38 @@ export const Header = () => {
 
     if (width > breakPoint) {
         return (
-            <nav className="flex justify-center items-center py-3 text-white">
-                <h1 className="text-4xl font-bold font-roboto ">RELAP</h1>
-                <input type="text" placeholder="Search" className="w-64 p-2 mx-6" />
-                <div className="text-lg mx-32">
-                    <a href="#" className="mx-5">Home</a>
-                    <a href="#" className="mx-5">Products</a>
-                    <a href="#" className="mx-5">Store</a>
-                    <a href="#" className="mx-5">Products</a>
-                    <a href="#" className="mx-5">News</a>
+            <div>
+                <nav className="flex justify-center items-center py-3 text-white">
+                    <h1 className="text-4xl font-bold font-roboto ">RELAP</h1>
+                    <input type="text" placeholder="Search" className="w-64 p-2 mx-6" />
+                    <div className="text-lg mx-32">
+                        <a href="#" className="mx-5">Home</a>
+                        <a href="#" className="mx-5">Products</a>
+                        <a href="#" className="mx-5">Store</a>
+                        <a href="#" className="mx-5">Products</a>
+                        <a href="#" className="mx-5">News</a>
+                    </div>
+                    <div className="flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mx-3">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                        </svg>
+                    </div>
+                </nav>
+                <div className="flex justify-center items-center py-6">
+                    <div>
+                        <img src={headerLaptop} alt="Laptop" />
+                    </div>
+                    <div className="text-white">
+                        <p className="text-sm">Welcome to</p>
+                        <h1 className="font-roboto font-bold text-6xl w-96 py-3">Relap Gadgets Store</h1>
+                        <button className="bg-amber-500 px-8 py-1 rounded-2xl text-lg">Store</button>
+                    </div>
                 </div>
-                <div className="flex">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mx-3">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                    </svg>
-                </div>
-            </nav>
+            </div>
+            
         )
     }
     
@@ -75,6 +89,17 @@ export const Header = () => {
                 <a href="#">Products</a>
                 <a href="#">News</a>
             </div>}
+
+            <div className="flex flex-col-reverse p-5">
+                    <div>
+                        <img src={headerLaptop} alt="Laptop" />
+                    </div>
+                    <div className="text-white mb-6">
+                        <p className="text-sm">Welcome to</p>
+                        <h1 className="font-roboto font-bold text-5xl w-96 py-3">Relap Gadgets Store</h1>
+                        <button className="bg-amber-500 px-8 py-1 rounded-2xl text-lg">Store</button>
+                    </div>
+                </div>
         </div>
         
     )
