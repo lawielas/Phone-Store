f51dfe80598d53d367f30954b98d07793dae261d
lawielas <lawie.aminu@yahoo.com>
2023-09-10 19:34:34 +0100

    Project setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+
+
+export const Header = () => {
+    return (
+        <nav>
+            <h1>RELAP</h1>
+            <input type="text" placeholder="Search" />
+            <div>
+                <a href="#">Home</a>
+                <a href="#">Products</a>
+                <a href="#">Store</a>
+                <a href="#">Products</a>
+                <a href="#">News</a>
+            </div>
+            <div>
+                
+            </div>
+        </nav>
+    )
+}
